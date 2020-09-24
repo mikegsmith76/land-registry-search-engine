8cacc0e7338a85edf7ad7811b3e96be635281d3e
Mike Smith <mail@mikegsmith.co.uk>
2020-09-24 20:18:32 +0100

    Further styling updates

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -7,24 +7,42 @@ use Twig\TwigFilter;
 
 class AppExtension extends AbstractExtension
 {
+    protected $propertyTypeOptions = [
+        "D" => "Detached",
+        "F" => "Flat",
+        "S" => "Semi Detached",
+        "T" => "Terraced",
+        "O" => "Other",
+    ];
+
+    protected $tenureOptions = [
+        "F" => "Freehold",
+        "L" => "Leasehold",
+    ];
+
     public function getFilters() : array
     {
         return [
+            new TwigFilter('format_property_type', [$this, 'formatPropertyType']),
             new TwigFilter('format_tenure', [$this, 'formatTenure']),
         ];
     }
 
+    public function formatPropertyType(string $propertyType) : string
+    {
+        if (empty($this->propertyTypeOptions[$propertyType])) {
+            return $propertyType;
+        }
+
+        return $this->propertyTypeOptions[$propertyType];
+    }
+
     public function formatTenure(string $tenure) : string
     {
-        $tenureOptions = [
-            "F" => "Freehold",
-            "L" => "Leasehold",
-        ];
-
-        if (empty($tenureOptions[$tenure])) {
+        if (empty($this->tenureOptions[$tenure])) {
             return $tenure;
         }
 
-        return $tenureOptions[$tenure];
+        return $this->tenureOptions[$tenure];
     }
 }
